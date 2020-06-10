de85474ad79d63bba1d797e815d2059e9b47411d
RaminHasibi <Ramin.Hasibi94@gmail.com>
2020-06-10 10:41:46 +0200

    some comment

--- .ipynb_checkpoints/train_VAE-checkpoint.py
+++ .ipynb_checkpoints/train_VAE-checkpoint.py
@@ -6,43 +6,8 @@ from torch_geometric.data import DataLoader
 from torch_geometric.nn import PointConv, fps, radius, global_max_pool
 from chamfer_distance import ChamferDistance
 from Data import ShapeNet_2048
+from train_AE import SAModule, GlobalSAModule, MLP
 
-
-class SAModule(torch.nn.Module):
-    def __init__(self, ratio, r, nn):
-        super(SAModule, self).__init__()
-        self.ratio = ratio
-        self.r = r
-        self.conv = PointConv(nn)
-
-    def forward(self, x, pos, batch, num_samples=32):
-        idx = fps(pos, batch, ratio=self.ratio)
-        row, col = radius(pos, pos[idx], self.r, batch, batch[idx],
-                          max_num_neighbors=num_samples)
-        edge_index = torch.stack([col, row], dim=0)
-        x = self.conv(x, (pos, pos[idx]), edge_index)
-        pos, batch = pos[idx], batch[idx]
-        return x, pos, batch
-
-
-class GlobalSAModule(torch.nn.Module):
-    def __init__(self, nn):
-        super(GlobalSAModule, self).__init__()
-        self.nn = nn
-
-    def forward(self, x, pos, batch):
-        x = self.nn(torch.cat([x, pos], dim=1))
-        x = global_max_pool(x, batch)
-        pos = pos.new_zeros((x.size(0), 3))
-        batch = torch.arange(x.size(0), device=batch.device)
-        return x, pos, batch
-
-
-def MLP(channels, batch_norm=True):
-    return Seq(*[
-        Seq(Lin(channels[i - 1], channels[i]), ReLU(), BN(channels[i]))
-        for i in range(1, len(channels))
-    ])
 
 class Net(torch.nn.Module):
     def __init__(self):
@@ -62,7 +27,7 @@ class Net(torch.nn.Module):
     
     
     def encode(self, x, batch):
-        sa1_out = self.sa1_module(data.x, data.x, data.batch)
+        sa1_out = self.sa1_module(x, x, batch)
         sa2_out = self.sa2_module(*sa1_out)
         sa3_out = self.sa3_module(*sa2_out)
         x, pos, batch = sa3_out
@@ -82,7 +47,7 @@ class Net(torch.nn.Module):
         return z
     
     def forward(self, data):
-        x ,  batch = data.x, data.batch
+        x,  batch = data.x, data.batch
         mu, logvar = self.encode(x, batch)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
@@ -93,11 +58,6 @@ def train():
     total_loss = 0
     step = 0
     for data in train_loader:
-        if step % 50 == 0:
-            print(step)
-            print('KLD:',KLD)
-            print('CHM:',CHM)
-        step += 1
         data = data.to(device)
         optimizer.zero_grad()
         out = model(data)
@@ -105,9 +65,14 @@ def train():
         CHM = (torch.mean(dist1)) + (torch.mean(dist2)) 
         KLD = -0.5 * torch.mean(
             torch.sum(1 + out[2] - out[1].pow(2) - out[2].exp(),dim=1))
+        loss = CHM + KLD
         loss.backward()
         total_loss += loss.item() * data.num_graphs
-        optimizer.step()
+        if step % 50 == 0:
+            print(step)
+            print('KLD:',KLD)
+            print('CHM:',CHM)
+        step += 1
     return total_loss / len(dataset)
 
 
